c5f1dc9abc2ebd88af9652a88858719d85f4f056
syfufo <616518277@qq.com>
2021-01-07 13:59:45 +0800

    修改实体类与import错误

--- src/main/java/seu/moyu/demo/booking/controller/ServerController.java
+++ src/main/java/seu/moyu/demo/booking/controller/ServerController.java
@@ -11,13 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import seu.moyu.demo.booking.entity.Hotel;
 import seu.moyu.demo.booking.entity.User;
 import seu.moyu.demo.booking.service.IHotelService;
 import seu.moyu.demo.booking.service.IOrderService;
 import seu.moyu.demo.booking.service.IRoomService;
 import seu.moyu.demo.booking.service.IUserService;
 
+import java.util.List;
 import javax.jws.soap.SOAPBinding;
+import java.sql.Date;
 import java.sql.Wrapper;
 
 /**
@@ -107,6 +110,72 @@ public class ServerController {
             e.printStackTrace();
             jsonObject.put("status", 500);
             jsonObject.put("msg", "修改失败！");
+        }
+        return jsonObject;
+    }
+    @RequestMapping(value = "/rate", method = RequestMethod.GET)
+    public JSONObject Rate(String token,int star,int orderID){
+        JSONObject jsonObject = new JSONObject();
+        try{
+            int res = orderService.RateOrder(token,star,orderID);
+            if(res == -1){
+                jsonObject.put("status", 501);
+                jsonObject.put("msg", "订单信息错误，请刷新！");
+            }
+            else if(res==-2){
+                jsonObject.put("status", 502);
+                jsonObject.put("msg", "该订单不能评价！");
+            }
+            else{
+                jsonObject.put("status", 200);
+            }
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            jsonObject.put("status", 500);
+            jsonObject.put("msg", "登录失效，请刷新！");
+        }
+        return jsonObject;
+    }
+
+    @RequestMapping(value = "/cancel", method = RequestMethod.GET)
+    public JSONObject Cancel(String token,int orderID){
+        JSONObject jsonObject = new JSONObject();
+        try{
+            int res = orderService.CancelOrder(token,orderID);
+            if(res == -1){
+                jsonObject.put("status", 501);
+                jsonObject.put("msg", "订单信息错误，请刷新！");
+            }
+            else if(res==-2){
+                jsonObject.put("status", 502);
+                jsonObject.put("msg", "该订单不能退订！");
+            }
+            else{
+                jsonObject.put("status", 200);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            jsonObject.put("status", 500);
+            jsonObject.put("msg", "登录失效，请刷新！");
+        }
+        return jsonObject;
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public JSONObject Search(String parameter, Date startTime, Date endTime, int customerNumber,String type, String location) {
+        JSONObject jsonObject = new JSONObject();
+        try{
+            List<Hotel> res = hotelService.Search(parameter);
+            jsonObject.put("status", 200);
+            jsonObject.put("List<Hotel>", res);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            jsonObject.put("status", 500);
+            jsonObject.put("msg", "登录失效，请刷新！");
         }
         return jsonObject;
     }
